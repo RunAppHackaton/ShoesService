6d7ec31972c6843992f000d6b7551d6b36848e7f
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-09-15 15:13:57 +0200

    first commit

---
+++ src/test/java/com/runapp/shoesservice/ShoesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runapp.shoesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

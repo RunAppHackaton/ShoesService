476ed4bc01e095b585d5ff0614d295625d5980b3
Denys Durbalov <116250966+denpoo1@users.noreply.github.com>
2024-03-09 17:23:54 +0100

    Update gcloud_deploy_to_cloud_run.sh

--- .github/workflows/gcloud_deploy_to_cloud_run.sh
+++ .github/workflows/gcloud_deploy_to_cloud_run.sh
@@ -4,7 +4,7 @@ gcloud run deploy $DOCKER_NAME_CONTAINER \
   --image=$GCP_REGION-docker.pkg.dev/$GCP_PROJECT_ID/$GCP_APP/$DOCKER_NAME_CONTAINER:$IMAGE_TAG \
   --region=$GCP_REGION \
   --project=$GCP_PROJECT_ID \
-  --port=$SHOES_SERVICE_SERVER_PORT \
+  --port=$SERVICE_SERVER_PORT \
   --add-cloudsql-instances=$SQL_INSTANCE_NAME \
   --service-account=$GCP_SQL_ACCOUNT_SERVICE_NAME \
   --vpc-connector=$VPC_CONNECTOR_NAME \
